3ca0a92510b90b9124f1b48c0899ed76480e2324
FebrianYudhis <killersvteam@gmail.com>
2021-04-05 19:25:49 +0700

    Menyelesaikan Menu Absen Masuk 2

--- resources/views/app/absen/datang.blade.php
+++ resources/views/app/absen/datang.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
+@push('scripts')
+<script src="{{ asset('vendor/app/js/time.js') }}"></script>
+<script src="{{ asset('vendor/sweetalert/sw2.min.js') }}"></script>
+@endpush
+
+@push('scripts')
+<script>
+    $('.konfirmasi').on('click',function(e){
+        let tulisan = $(this).text();
+        console.log(tulisan);
+        e.preventDefault();
+        var form = $(this).parents('form');
+        console.log(form);
+        
+        Swal.fire({
+            icon: 'warning',
+            title: tulisan,
+            text: "Apakah anda yakin ?",
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya'
+        }).then((result) => {
+            if (result.value) {
+                form.submit();
+            }
+        });
+    });
+</script>
+@endpush
+
 @section('konten')
 <div class="card">
     <div class="card-header">Absen Datang</div>
@@ -8,13 +39,16 @@
             @csrf
             @method('put')
             <div class="form-group">
-                <label for="tanggal">Tanggal :</label>
+                <label for="tanggal">Shift :</label>
                 <input type="date" class="form-control" id="tanggal" name="tanggal" value="{{ old('tanggal') }}">
                 @error('tanggal')
                 <div class="text-danger mt-2">{{ $message }}</div>
                 @enderror
             </div>
-            <button type="submit" class="btn btn-primary w-100">Sudah Datang</button>
+            <p class="mb-4 text-danger font-italic">Anda Akan Absen Pada <span id="hariini"
+                    class="font-weight-bold"></span> Untuk Shift Tanggal <span id="tanggalshift"
+                    class="font-weight-bold">-</span></p>
+            <button type="submit" class="btn btn-primary w-100 konfirmasi">Sudah Datang</button>
         </form>
     </div>
 </div>
