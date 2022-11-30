29b88b85d8d776d004d408c0637db4207bda04e3
FebrianYudhis <killersvteam@gmail.com>
2022-11-30 11:44:49 +0700

    Memperbaiki Struktur Database Admin

--- database/migrations/2021_03_30_014009_create_admins_table.php
+++ database/migrations/2021_03_30_014009_create_admins_table.php
@@ -15,7 +15,7 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('nama');
             $table->string('password');
             $table->rememberToken();
